7f76891822b87af4b60e7bceb22eacb2c4bc6b11
VIXXPARK <vixx170627@gmail.com>
2022-01-02 20:36:55 +0900

    복합키 (day, routine_id)로 설정 first()로 했을 때 subQuery 발생하는 것을 확인 그래서 임시로 all()로 변환

--- routine/routineRouters.py
+++ routine/routineRouters.py
@@ -2,18 +2,19 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from base.database.database import get_db
-from routine.repository.routineRepository import create_routine
+from routine.repository.routineRepository import create_routine, get_routine
 from routine.schemas import RoutineCreateRequest
 
 router = APIRouter(prefix='/api/v1/routine', tags=['routines'])
 
 
-@router.get('')
-async def get_routine():
-    return 'routine'
+@router.get('/{routine_id}')
+def get_routine_router(routine_id: int, db: Session = Depends(get_db)):
+    routine = get_routine(db, routine_id)
+    return routine
 
 
 @router.post('/create')
-async def create_routine_router(routine: RoutineCreateRequest, db: Session = Depends(get_db)):
+def create_routine_router(routine: RoutineCreateRequest, db: Session = Depends(get_db)):
     save_routine = create_routine(db, routine)
     return save_routine
