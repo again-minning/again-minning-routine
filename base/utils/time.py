29be0ccbe9f43bf122b51e1f758afc6b4f06a5eb
VIXXPARK <vixx170627@gmail.com>
2022-01-07 09:16:55 +0900

    날짜 계산하는 클래스 생성 다음 이슈에서 테스트케이스 작성할 예정

--- base/utils/time.py
+++ base/utils/time.py
@@ -1,4 +1,5 @@
 from dateutil.parser import parse, ParserError
+from datetime import timedelta
 
 
 def convert_str2time(time: str):
@@ -15,3 +16,63 @@ def convert_str2date(date: str):
         return date.date()
     except ParserError:
         return None
+
+
+class EndDay:
+    __MONTH = {
+        1: 31,
+        2: 28,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31,
+    }
+
+    def check_leap_year(self, year):
+        if ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0):
+            self.__MONTH[2] = 29
+        else:
+            self.__MONTH[2] = 28
+
+    def end_of_month(self, year, month):
+        self.check_leap_year(year)
+        if type(month) is str and month.isdigit():
+            pass
+        elif type(month) is int:
+            pass
+        else:
+            raise ValueError('숫자로 된 값을 넣어주세요')
+
+        if month == 0:
+            month = 12
+
+        if month == 13:
+            month = 1
+
+        if not (1 <= int(month) <= 12):
+            raise ValueError(' 1 이상 12 이하의 값을 넣어주세요')
+
+        return self.__MONTH[month]
+
+    def prev_end_of_month(self, year, month):
+        return self.end_of_month(year, month-1)
+
+    def next_end_of_month(self, year, month):
+        return self.end_of_month(year, month+1)
+
+    def return_prev_start_date(self, date):
+        return (date - timedelta(days=self.prev_end_of_month(year=date.year, month=date.month))).replace(day=1)
+
+    def return_prev_end_date(self,date):
+        return (
+                date - timedelta(days=self.prev_end_of_month(year=date.year, month=date.month))
+                ).replace(day=self.prev_end_of_month(year=date.year, month=date.month))
+
+    def return_prev_between_date(self, date):
+        return self.return_prev_start_date(date), self.return_prev_end_date(date)
