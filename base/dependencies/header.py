7f9195db41a0f61ff211b34f6ed7da9bfc505898
VIXXPARK <vixx170627@gmail.com>
2022-01-31 17:08:50 +0900

    에러 코드 상수화

--- base/dependencies/header.py
+++ base/dependencies/header.py
@@ -1,8 +1,8 @@
 from fastapi import Header
-
-from base.exception.exception import NotFoundException
+from base.constants.base_message import USER_NOT_FOUND
+from base.exception.exception import MinningException
 
 
 def check_account_header(account: str = Header(...)):
     if not account:
-        raise NotFoundException('유저 정보가 존재하지 않습니다.')
+        raise MinningException(USER_NOT_FOUND)
