80fd553967a6cab2e5b07b0ba0aa866b087dfce3
VIXXPARK <vixx170627@gmail.com>
2022-01-31 10:29:00 +0900

    업데이트 관련 로직이 수행할 때 modified_date도 같이 업데이트 되도록 수정

--- routine/models/routine.py
+++ routine/models/routine.py
@@ -61,6 +61,7 @@ class Routine(BaseColumnMixin, TimestampMixin, Base):
         added = request_days - retained
         self.add_days(added)
         self.delete_days(db, deleted)
+        self.update_modified_at()
 
     def delete_days(self, db, deleted):
         for delete_day in deleted:
@@ -81,3 +82,4 @@ class Routine(BaseColumnMixin, TimestampMixin, Base):
         self.start_time = convert_str2time(request.start_time)
         self.goal = request.goal
         self.is_alarm = request.is_alarm
+        self.update_modified_at()
