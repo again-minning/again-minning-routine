dd338700656f7a5a8d0fad0f581f4cd0a3e48b24
VIXXPARK <vixx170627@gmail.com>
2022-02-05 09:29:39 +0900

    테스트에서는 잘 되었는데 포스트맨으로 테스트한 결과 에러나는 것 확인 그래서 롤백 결정

--- retrospect/service/retrospect_service.py
+++ retrospect/service/retrospect_service.py
@@ -23,9 +23,9 @@ def create_retrospect(db: Session, routine_id: int, content: str, date: str, ima
         contains_eager(RoutineDay.routine)
     ).filter(
         and_(
-            RoutineDay.routine_id.is_(routine_id),
-            RoutineDay.day.is_(weekday),
-            Routine.account_id.is_(account)
+            RoutineDay.routine_id == routine_id,
+            RoutineDay.day == weekday,
+            Routine.account_id == account
         )
     ).first()
 
@@ -47,8 +47,8 @@ def __check_retrospect(date, db, routine_days, routine_id):
         Retrospect
     ).filter(
         and_(
-            Retrospect.scheduled_date.is_(date),
-            Retrospect.routine_id.is_(routine_id)
+            Retrospect.scheduled_date == date,
+            Retrospect.routine_id == routine_id
         )
     ).exists()
     is_exists_retrospects = db.query(scheduled_retrospect).scalar()
@@ -62,8 +62,8 @@ def get_detail_retrospect(db: Session, retrospect_id: int, account: int):
     ).options(
         joinedload(Retrospect.image)
     ).filter(
-        Retrospect.id.is_(retrospect_id),
-        Retrospect.account_id.is_(account)
+        Retrospect.id == retrospect_id,
+        Retrospect.account_id == account
     ).first()
 
     if not retrospect:
@@ -77,8 +77,8 @@ def put_detail_retrospect(retrospect_id: int, content: str, image: UploadFile, d
     ).options(
         joinedload(Retrospect.image)
     ).filter(
-        Retrospect.id.is_(retrospect_id),
-        Retrospect.account_id.is_(account)
+        Retrospect.id == retrospect_id,
+        Retrospect.account_id == account
     ).first()
     if not retrospect:
         raise MinningException(RETROSPECT_NOT_FOUND_ID)
@@ -97,8 +97,8 @@ def delete_detail_retrospect(retrospect_id: int, db: Session, account: int):
         Retrospect
     ).filter(
         and_(
-            Retrospect.id.is_(retrospect_id),
-            Retrospect.account_id.is_(account)
+            Retrospect.id == retrospect_id,
+            Retrospect.account_id == account
         )
     ).first()
     if not retrospect:
